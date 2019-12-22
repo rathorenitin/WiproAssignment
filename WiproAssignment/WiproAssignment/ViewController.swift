66bbdf6313f81e6fcfc578ef2ad1a8b92c914a59
Nitin Singh <nitin.singh@appinventiv.com>
2019-12-22 13:46:09 +0530

    # initial project setup

---
+++ WiproAssignment/WiproAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WiproAssignment
+//
+//  Created by Apple on 22/12/19.
+//  Copyright © 2019 Nitin Singh Rathore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
